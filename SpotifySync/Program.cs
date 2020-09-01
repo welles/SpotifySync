43c1080f771934d11094482c692c9e87a70deeb1
Nico Welles <nico@welles.io>
2020-09-01 22:14:37 +0200

    Try getting token from secrets

--- SpotifySync/Program.cs
+++ SpotifySync/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SpotifyAPI.Web;
 
 namespace SpotifySync
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var token = Environment.GetEnvironmentVariable("SPOTIFY_TOKEN");
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new InvalidOperationException("SPOTIFY_TOKEN environment variable is null or empty.");
+            }
+
+            var pkceToken = JsonConvert.DeserializeObject<PKCETokenResponse>(token, new JsonSerializerSettings { Error = (sender, args) => throw args.ErrorContext.Error});
+
+            if (pkceToken == null)
+            {
+                throw new InvalidOperationException("PKCE Token is null.");
+            }
+
+            Console.WriteLine("Scopes: " + pkceToken.Scope);
         }
     }
 }
