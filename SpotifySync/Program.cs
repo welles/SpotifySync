6aa7e4dbd3bfcfc9bc7ff01dabb3f5678c48a9d4
Nico Welles <nico@welles.io>
2020-09-01 23:47:40 +0200

    Add basic authorization workflow

--- SpotifySync/Program.cs
+++ SpotifySync/Program.cs
@@ -1,29 +1,143 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SpotifyAPI.Web;
+using SpotifySync.Models;
 
 namespace SpotifySync
 {
     public static class Program
     {
+        private const string SpotifyTokenName = "SPOTIFY_TOKEN";
+
+        private const string SpotifyClientIdName = "SPOTIFY_CLIENT_ID";
+
+        private const string GitHubTokenName = "GITHUB_TOKEN";
+
+        private const string GitHubRepositoryName = "GITHUB_REPOSITORY";
+
+        private static readonly string SpotifyClientId = Environment.GetEnvironmentVariable(Program.SpotifyClientIdName);
+
+        private static readonly string SpotifyToken = Environment.GetEnvironmentVariable(Program.SpotifyTokenName);
+
+        private static readonly string GitHubToken = Environment.GetEnvironmentVariable(Program.GitHubTokenName);
+
+        private static readonly string GitHubRepository = Environment.GetEnvironmentVariable(Program.GitHubRepositoryName);
+
         public static async Task Main()
         {
-            var token = Environment.GetEnvironmentVariable("SPOTIFY_TOKEN");
-
-            if (string.IsNullOrWhiteSpace(token))
+            if (string.IsNullOrWhiteSpace(Program.SpotifyToken))
             {
                 throw new InvalidOperationException("SPOTIFY_TOKEN environment variable is null or empty.");
             }
 
-            var pkceToken = JsonConvert.DeserializeObject<PKCETokenResponse>(token, new JsonSerializerSettings { Error = (sender, args) => throw args.ErrorContext.Error});
-
-            if (pkceToken == null)
+            if (string.IsNullOrWhiteSpace(Program.SpotifyClientId))
             {
-                throw new InvalidOperationException("PKCE Token is null.");
+                throw new InvalidOperationException("SPOTIFY_CLIENT_ID environment variable is null or empty.");
             }
 
-            Console.WriteLine("Scopes: " + pkceToken.Scope);
+            if (string.IsNullOrWhiteSpace(Program.GitHubToken))
+            {
+                throw new InvalidOperationException("GITHUB_TOKEN environment variable is null or empty.");
+            }
+
+            if (string.IsNullOrWhiteSpace(Program.GitHubRepository))
+            {
+                throw new InvalidOperationException("GITHUB_REPOSITORY environment variable is null or empty.");
+            }
+
+            var publicKey = await Program.GetPublicKey();
+
+            var token = Program.GetToken();
+
+            var authenticator = new PKCEAuthenticator(Program.SpotifyClientId, token);
+            authenticator.TokenRefreshed += async (sender, newToken) => await Program.UpdateSecret(publicKey, newToken);
+
+            var config = SpotifyClientConfig.CreateDefault()
+                .WithAuthenticator(authenticator);
+
+            var spotify = new SpotifyClient(config);
+
+            var me = await spotify.UserProfile.Current();
+            Console.WriteLine($"Authentication successful. ({me.DisplayName})");
+        }
+
+        private static PKCETokenResponse GetToken()
+        {
+            var pkceToken = JsonConvert.DeserializeObject<PKCETokenResponse>(Program.SpotifyToken);
+
+            return pkceToken;
+        }
+
+        private static async Task<GitHubPublicKey> GetPublicKey()
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/repos/" + Program.GitHubRepository + "/actions/secrets/public-key"))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Program.GitHubToken);
+                request.Headers.UserAgent.Add(new ProductInfoHeaderValue("SpotifySync", "1.0"));
+
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var publicKey = JsonConvert.DeserializeObject<GitHubPublicKey>(content);
+
+                    return publicKey;
+                }
+            }
+
+            throw new InvalidOperationException("Could not load public key from GitHub.");
+        }
+
+        private static GitHubSecretPayload GetSecretPayload(GitHubPublicKey publicKey, PKCETokenResponse token)
+        {
+            var tokenJson = JsonConvert.SerializeObject(token, Formatting.None);
+
+            var secretValueBytes = Encoding.UTF8.GetBytes(tokenJson);
+            var publicKeyBytes = Convert.FromBase64String(publicKey.Key);
+
+            var sealedPublicKeyBox = Sodium.SealedPublicKeyBox.Create(secretValueBytes, publicKeyBytes);
+
+            var keyboxString = Convert.ToBase64String(sealedPublicKeyBox);
+
+            var payload = new GitHubSecretPayload
+            {
+                EncryptedValue = keyboxString,
+                KeyId = publicKey.KeyId
+            };
+
+            return payload;
+        }
+
+        private static async Task UpdateSecret(GitHubPublicKey publicKey, PKCETokenResponse token)
+        {
+            var payload = Program.GetSecretPayload(publicKey, token);
+
+            var payloadJson = JsonConvert.SerializeObject(payload, Formatting.None);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage(HttpMethod.Put, "https://api.github.com/repos/" + Program.GitHubRepository + "/actions/secrets/" + Program.SpotifyTokenName))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Program.GitHubToken);
+                request.Headers.UserAgent.Add(new ProductInfoHeaderValue("SpotifySync", "1.0"));
+
+                request.Content = new StringContent(payloadJson);
+
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException("Could not update secret value on GitHub.");
         }
     }
 }
