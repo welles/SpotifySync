17ffb1fc889b68fc2a5ec97e89f9619863fe0b7c
Nico Welles <nico@welles.io>
2020-09-13 23:39:34 +0200

    Back to the basics

--- SpotifySync/Program.cs
+++ SpotifySync/Program.cs
@@ -1,277 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Sheets.v4;
-using Google.Apis.Sheets.v4.Data;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SpotifyAPI.Web;
-using SpotifySync.Models;
 
 namespace SpotifySync
 {
     public static class Program
     {
-        private const string SpotifyTokenName = "SPOTIFY_TOKEN";
-
-        private const string SpotifyClientIdName = "SPOTIFY_CLIENT_ID";
-
-        private const string SpotifyPlaylistIdName = "SPOTIFY_PLAYLIST_ID";
-
-        private const string GitHubTokenName = "GH_TOKEN";
-
-        private const string GitHubRepositoryName = "GITHUB_REPOSITORY";
-
-        private const string GoogleTokenName = "GOOGLE_TOKEN";
-
-        private const string GoogleSheetIdName = "GOOGLE_SHEET_ID";
-
-
-        private static readonly string SpotifyClientId = Environment.GetEnvironmentVariable(Program.SpotifyClientIdName);
-
-        private static readonly string SpotifyPlaylistId = Environment.GetEnvironmentVariable(Program.SpotifyPlaylistIdName);
-
-        private static readonly string SpotifyToken = Environment.GetEnvironmentVariable(Program.SpotifyTokenName);
-
-        private static readonly string GitHubToken = Environment.GetEnvironmentVariable(Program.GitHubTokenName);
-
-        private static readonly string GitHubRepository = Environment.GetEnvironmentVariable(Program.GitHubRepositoryName);
-
-        private static readonly string GoogleToken = Environment.GetEnvironmentVariable(Program.GoogleTokenName);
-
-        private static readonly string GoogleSheetId = Environment.GetEnvironmentVariable(Program.GoogleSheetIdName);
-
         public static async Task Main()
         {
-            Program.CheckEnvVariable(Program.SpotifyToken, Program.SpotifyTokenName);
-            Program.CheckEnvVariable(Program.SpotifyClientId, Program.SpotifyClientIdName);
-            Program.CheckEnvVariable(Program.SpotifyPlaylistId, Program.SpotifyPlaylistIdName);
-            Program.CheckEnvVariable(Program.GitHubToken, Program.GitHubTokenName);
-            Program.CheckEnvVariable(Program.SpotifyToken, Program.SpotifyTokenName);
-            Program.CheckEnvVariable(Program.GitHubRepository, Program.GitHubRepositoryName);
-            Program.CheckEnvVariable(Program.GoogleToken, Program.GoogleTokenName);
-            Program.CheckEnvVariable(Program.GoogleSheetId, Program.GoogleSheetIdName);
+            Console.Write("Loading environment variables... ");
 
-            var publicKey = await Program.GetPublicKey();
+            var spotifyClientId = Program.GetEnvironmentVariable("SPOTIFY_CLIENT_ID");
+            var spotifyClientSecret = Program.GetEnvironmentVariable("SPOTIFY_CLIENT_SECRET");
+            var spotifyRefreshToken = Program.GetEnvironmentVariable("SPOTIFY_REFRESH_TOKEN");
 
-            var token = Program.GetToken();
+            Console.WriteLine("[Ok]");
 
-            var authenticator = new PKCEAuthenticator(Program.SpotifyClientId, token);
-            authenticator.TokenRefreshed += async (sender, newToken) => await Program.UpdateSecret(publicKey, newToken);
+            Console.Write("Loading Spotify token... ");
 
-            var config = SpotifyClientConfig.CreateDefault().WithAuthenticator(authenticator);
+            var spotifyToken = await Program.GetSpotifyToken(spotifyClientId, spotifyClientSecret, spotifyRefreshToken);
 
-            var spotify = new SpotifyClient(config);
+            Console.WriteLine("[Ok]");
 
-            var me = await spotify.UserProfile.Current();
+            Console.Write("Authenticating with Spotify... ");
 
-            Console.WriteLine($"Authentication successful. ({me.DisplayName})");
+            var spotifyClient = await Program.GetSpotifyClient(spotifyToken);
 
-            var librarySongsTask = (await spotify.Library.GetTracks()).Paginate(spotify);
-
-            var playlistItems = await (await spotify.Playlists.GetItems(Program.SpotifyPlaylistId)).Paginate(spotify);
-            var playlistSongs = playlistItems.Select(x => x.Track).Cast<FullTrack>().ToList();
-
-            var librarySongs = await librarySongsTask;
-            librarySongs.Reverse();
-
-            Console.WriteLine($"There are {librarySongs.Count} songs in the library.");
-            Console.WriteLine($"There are {playlistSongs.Count} songs in the playlist.");
-
-            await Program.SynchronizeSongs(spotify, librarySongs, playlistSongs);
+            Console.WriteLine("[Ok]");
         }
 
-        private static async Task<List<T>> Paginate<T>(this IPaginatable<T> pages, ISpotifyClient spotify)
-            where T : class
-        {
-            var items = new List<T>();
-
-            await foreach(var item in spotify.Paginate(pages))
-            {
-                items.Add(item);
-
-                await Task.Delay(10);
-            }
-
-            return items;
-        }
-
-        private static void CheckEnvVariable(string variable, string variableName)
-        {
-            if (string.IsNullOrWhiteSpace(variable))
-            {
-                throw new InvalidOperationException($"{variableName} environment variable is null or empty.");
-            }
-        }
-
-        private static async Task SynchronizeSongs(SpotifyClient spotify, IList<SavedTrack> librarySongs, IList<FullTrack> playlistSongs)
-        {
-            var libraryIds = librarySongs.Select(x => x.Track.Id).ToList();
-            var playlistIds = playlistSongs.Select(x => x.Id).ToList();
-
-            var added = libraryIds.Except(playlistIds).ToList();
-            var removed = playlistIds.Except(libraryIds).ToList();
-
-            Console.WriteLine($"{added.Count} songs were added since last run.");
-            Console.WriteLine($"{removed.Count} songs were removed since last run.");
-
-            var addedSongs = librarySongs.Where(x => added.Contains(x.Track.Id)).ToList();
-            var removedSongs = playlistSongs.Where(x => removed.Contains(x.Id)).ToList();
-
-            for (var index = 0; index < addedSongs.Count; index += 100)
-            {
-                var tracks = addedSongs.Skip(index).Take(100).ToList();
-
-                var uris = tracks.Select(x => x.Track.Uri).ToList();
-
-                await spotify.Playlists.AddItems(Program.SpotifyPlaylistId, new PlaylistAddItemsRequest(uris));
-
-                Console.WriteLine($"Added {tracks.Count} songs to playlist.");
-            }
-
-            for (var index = 0; index < removedSongs.Count; index += 100)
-            {
-                var tracks = removedSongs.Skip(index).Take(100).ToList();
-
-                var uris = tracks.Select(x => new PlaylistRemoveItemsRequest.Item {Uri = x.Uri}).ToList();
-
-                await spotify.Playlists.RemoveItems(Program.SpotifyPlaylistId, new PlaylistRemoveItemsRequest {Tracks = uris});
-
-                Console.WriteLine($"Removed {tracks.Count} songs from playlist.");
-            }
-
-            await Program.LogChanges(librarySongs, addedSongs, removedSongs);
-        }
-
-        private static async Task LogChanges(IList<SavedTrack> librarySongs, IList<SavedTrack> addedSongs, IList<FullTrack> removedSongs)
-        {
-            var service = Program.CreateSheetsService();
-
-            await Program.UpdateCurrentSheet(librarySongs, service);
-
-            await Program.AppendAddedLog(addedSongs, service);
-
-            await Program.AppendRemovedLog(removedSongs, service);
-        }
-
-        private static async Task UpdateCurrentSheet(IList<SavedTrack> librarySongs, SheetsService service)
-        {
-            var serviceValues = service.Spreadsheets.Values;
-
-            var clear = serviceValues.Clear(new ClearValuesRequest(), Program.GoogleSheetId, "Current!A:E");
-
-            await clear.ExecuteAsync();
-
-            var rows = librarySongs.Select(song => new[]
-            {
-                $"=IMAGE(\"{song.Track.Album.Images.OrderByDescending(x => x.Height).First().Url}\")",
-                song.Track.Name,
-                song.Track.Artists.First().Name,
-                song.Track.Album.Name,
-                song.Track.Id
-            }).ToArray();
-
-            var valueRange = new ValueRange {Values = rows };
-
-            var update = serviceValues.Update(valueRange, Program.GoogleSheetId, "Current!A:E");
-            update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
-
-            await update.ExecuteAsync();
-
-            Console.WriteLine($"Saved current songs list to Google Sheet.");
-        }
-
-        private static async Task AppendAddedLog(IList<SavedTrack> addedSongs, SheetsService service)
-        {
-            var serviceValues = service.Spreadsheets.Values;
-
-            var addedRows = addedSongs.Select(song => new[]
-            {
-                "Added",
-                $"=IMAGE(\"{song.Track.Album.Images.OrderByDescending(x => x.Height).First().Url}\")",
-                song.Track.Name,
-                song.Track.Artists.First().Name,
-                song.Track.Album.Name,
-                song.Track.Id
-            }).ToArray();
-
-            var addedValueRange = new ValueRange {Values = addedRows };
-
-            var update = serviceValues.Append(addedValueRange, Program.GoogleSheetId, "Log!A:F");
-            update.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-
-            await update.ExecuteAsync();
-
-            Console.WriteLine($"Saved added songs to Google Sheet.");
-        }
-
-        private static async Task AppendRemovedLog(IList<FullTrack> removedSongs, SheetsService service)
-        {
-            var serviceValues = service.Spreadsheets.Values;
-
-            var removedRows = removedSongs.Select(song => new[]
-            {
-                "Removed",
-                $"=IMAGE(\"{song.Album.Images.OrderByDescending(x => x.Height).First().Url}\")",
-                song.Name,
-                song.Artists.First().Name,
-                song.Album.Name,
-                song.Id
-            }).ToArray();
-
-            var removedValueRange = new ValueRange {Values = removedRows };
-
-            var update = serviceValues.Append(removedValueRange, Program.GoogleSheetId, "Log!A:F");
-            update.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-
-            await update.ExecuteAsync();
-
-            Console.WriteLine($"Saved removed songs to Google Sheet.");
-        }
-
-        private static SheetsService CreateSheetsService()
-        {
-            using (var stream = Program.GoogleToken.ToStream())
-            {
-                var serviceInitializer = new BaseClientService.Initializer
-                {
-                    HttpClientInitializer = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.Spreadsheets)
-                };
-
-                return new SheetsService(serviceInitializer);
-            }
-        }
-
-        private static Stream ToStream(this string s)
-        {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
-
-        private static PKCETokenResponse GetToken()
-        {
-            var pkceToken = JsonConvert.DeserializeObject<PKCETokenResponse>(Program.SpotifyToken);
-
-            return pkceToken;
-        }
-
-        private static async Task<GitHubPublicKey> GetPublicKey()
+        private static async Task<string> GetSpotifyToken(string spotifyClientId, string spotifyClientSecret, string spotifyRefreshToken)
         {
             using (var client = new HttpClient())
-            using (var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/repos/" + Program.GitHubRepository + "/actions/secrets/public-key"))
+            using (var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token"))
             {
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Program.GitHubToken);
-                request.Headers.UserAgent.Add(new ProductInfoHeaderValue("SpotifySync", "1.0"));
+                var spotifyClientString = spotifyClientId + ":" + spotifyClientSecret;
+                var spotifyClientBytes = Encoding.UTF8.GetBytes(spotifyClientString);
+                var spotifyClientStringEncoded = Convert.ToBase64String(spotifyClientBytes);
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Basic", spotifyClientStringEncoded);
+
+                var body = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("grant_type", "refresh_token"),
+                    new KeyValuePair<string, string>("refresh_token", spotifyRefreshToken)
+                };
+
+                request.Content = new FormUrlEncodedContent(body);
 
                 var response = await client.SendAsync(request);
 
@@ -279,58 +59,41 @@ namespace SpotifySync
                 {
                     var content = await response.Content.ReadAsStringAsync();
 
-                    var publicKey = JsonConvert.DeserializeObject<GitHubPublicKey>(content);
+                    var token = JObject.Parse(content).Value<string>("access_token");
 
-                    return publicKey;
+                    return token;
                 }
             }
 
-            throw new InvalidOperationException("Could not load public key from GitHub.");
+            throw new InvalidOperationException("Could not load Spotify token.");
         }
 
-        private static GitHubSecretPayload GetSecretPayload(GitHubPublicKey publicKey, PKCETokenResponse token)
+        private static string GetEnvironmentVariable(string key)
         {
-            var tokenJson = JsonConvert.SerializeObject(token, Formatting.None);
+            var variable = Environment.GetEnvironmentVariable(key);
 
-            var secretValueBytes = Encoding.UTF8.GetBytes(tokenJson);
-            var publicKeyBytes = Convert.FromBase64String(publicKey.Key);
-
-            var sealedPublicKeyBox = Sodium.SealedPublicKeyBox.Create(secretValueBytes, publicKeyBytes);
-
-            var keyboxString = Convert.ToBase64String(sealedPublicKeyBox);
-
-            var payload = new GitHubSecretPayload
+            if (string.IsNullOrWhiteSpace(variable))
             {
-                EncryptedValue = keyboxString,
-                KeyId = publicKey.KeyId
-            };
-
-            return payload;
-        }
-
-        private static async Task UpdateSecret(GitHubPublicKey publicKey, PKCETokenResponse token)
-        {
-            var payload = Program.GetSecretPayload(publicKey, token);
-
-            var payloadJson = JsonConvert.SerializeObject(payload, Formatting.None);
-
-            using (var client = new HttpClient())
-            using (var request = new HttpRequestMessage(HttpMethod.Put, "https://api.github.com/repos/" + Program.GitHubRepository + "/actions/secrets/" + Program.SpotifyTokenName))
-            {
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Program.GitHubToken);
-                request.Headers.UserAgent.Add(new ProductInfoHeaderValue("SpotifySync", "1.0"));
-
-                request.Content = new StringContent(payloadJson);
-
-                var response = await client.SendAsync(request);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    return;
-                }
+                throw new ArgumentException($"Environment variable \"{key}\" is not set.", key);
             }
 
-            throw new InvalidOperationException("Could not update secret value on GitHub.");
+            return variable;
+        }
+
+        private static async Task<SpotifyClient> GetSpotifyClient(string spotifyToken)
+        {
+            var config = SpotifyClientConfig.CreateDefault().WithToken(spotifyToken, "Bearer");
+
+            var spotify = new SpotifyClient(config);
+
+            var me = await spotify.UserProfile.Current();
+
+            if (string.IsNullOrWhiteSpace(me.Id))
+            {
+                throw new Exception("Authorization with Spotify failed.");
+            }
+
+            return spotify;
         }
     }
 }
